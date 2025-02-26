fef5c1a889bb2cbeaac10f0a2d957b872fbf95ae
UShamila <s.uwineza@alustudent.com>
2025-02-26 01:24:15 -0800

    Printing and Using Variables

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Define a variable to store the name
+name = "Shamila"
+
+# Print "Hello, World!"
+print("Hello, World!")
+
+# Print a personalized greeting using the name variable
+print(f"Hello, {name}!")
+
