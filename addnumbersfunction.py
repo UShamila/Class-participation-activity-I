5d1ffb53d8820579ff256c1bcd01ec5505b13936
UShamila <s.uwineza@alustudent.com>
2025-02-26 01:26:04 -0800

    Creating a Function to Add Two Numbers

---
+++ addnumbersfunction.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Ask the user to input two numbers
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Add the two numbers and display the result
+result = a + b
+print(f"The sum of {a} and {b} is {result}")
